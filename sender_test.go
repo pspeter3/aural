6713537efb1a51ca1fd521a04672931bce77f413
Phips Peter <pspeter333@gmail.com>
2016-05-06 21:23:15 -0700

    [Messenger] Change UserID to uint64
    The Facebook API made it seem like the IDs were strings but they are
    numbers instead.

--- sender_test.go
+++ sender_test.go
@@ -12,14 +12,14 @@ func TestSender_Send(t *testing.T) {
 	token := "token"
 	sender := aural.NewSender(token)
 	user := aural.User{
-		ID: aural.UserID("1"),
+		ID: aural.UserID(1),
 	}
 	m := mockDoer{http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		equals(t, r.URL.Path, "/v2.6/me/messages")
 		equals(t, r.URL.Query().Get("access_token"), token)
 		body, err := ioutil.ReadAll(r.Body)
 		ok(t, err)
-		expected := `{"recipient":{"id":"1"},"message":{"attachment":{"type":"template","payload":{"template_type":"generic","elements":[]}}}}`
+		expected := `{"recipient":{"id":1},"message":{"attachment":{"type":"template","payload":{"template_type":"generic","elements":[]}}}}`
 		assert(t, strings.EqualFold(string(body), expected), "Bodies are not the same")
 	})}
 	sender.Send(m, user, []aural.Element{})
