ffd89e3d4c32f8f8c0d510d302e3f3a7cd2fb024
Phips Peter <pspeter333@gmail.com>
2016-05-06 20:22:09 -0700

    [Heroku] Fix pointer issues
    Fix string issues.

--- cmd/aural/main.go
+++ cmd/aural/main.go
@@ -12,10 +12,10 @@ func main() {
 	addr := flag.String("addr", ":3000", "Address to bind")
 	verify := flag.String("verify", "", "Facebook Verify Token")
 	flag.Parse()
-	sender := aural.NewSender(access)
+	sender := aural.NewSender(*access)
 	dispatcher := aural.NewDispatcher(func() aural.Doer {
 		return &http.Client{}
 	}, sender)
-	verifier := aural.NewVerifier(verify)
+	verifier := aural.NewVerifier(*verify)
 	log.Fatal(http.ListenAndServe(*addr, aural.New(dispatcher, verifier)))
 }
