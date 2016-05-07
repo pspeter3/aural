b448f72c3c60ec8abb9c49d5474bdb3585680407
Phips Peter <pspeter3@users.noreply.github.com>
2016-05-06 23:55:10 -0700

    [Heroku] Dump requests (#17)
    This will dump the request body so that the code can be debugged locally

--- dispatcher.go
+++ dispatcher.go
@@ -25,6 +25,7 @@ func (d dispatcher) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
+	log.Printf("%v\n", data)
 	var wg sync.WaitGroup
 	for _, entry := range data.Entries {
 		for _, messaging := range entry.Messagings {
